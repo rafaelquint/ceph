5634739938b400ec568862bf83cee55040cb1486
Rafael Quintero <rafaelq@bu.edu>
2019-07-31 16:53:00 -0400

    mgr/dashboard: Verify fields on OSDs page
    Fixes: https://tracker.ceph.com/issues/41041
    
    Signed-off-by: Adam King <adking@redhat.com>
    Signed-off-by: Rafael Quintero <rquinter@redhat.com>

--- src/pybind/mgr/dashboard/frontend/e2e/cluster/osds.e2e-spec.ts
+++ src/pybind/mgr/dashboard/frontend/e2e/cluster/osds.e2e-spec.ts
@@ -1,3 +1,4 @@
+import { $$, browser, by, element } from 'protractor';
 import { Helper } from '../helper.po';
 
 describe('OSDs page', () => {
@@ -31,5 +32,44 @@ describe('OSDs page', () => {
     it('should show overall performance as a second tab', () => {
       expect(osds.getTabText(1)).toEqual('Overall Performance');
     });
+  });
+
+    describe('check existence of fields on OSD page', () => {
+      it('should check that number of rows and count in footer match', () => {
+        osds.navigateTo();
+        osds.getTableCount().getText().then((tableCount) => {
+          tableCount = tableCount.slice(13, -6); // Grabs number of hosts from table footer
+          expect(osds.getTableRows().count()).toMatch(tableCount);
+        });
+      });
+      it('should verify that selected footer increases when an entry is clicked', () => {
+        osds.navigateTo();
+        $$('.datatable-body-cell-label')
+          .first()
+          .click(); // clicks first osd
+        osds.getTableCount().getText().then((tableCount) => {
+          tableCount = tableCount.substring(0, 1); // Grabs number of hosts from table footer
+          expect(tableCount).toMatch('1');
+        });
+      });
+      it('should verify that buttons exist', () => {
+        osds.navigateTo();
+        expect(element(by.cssContainingText('button', 'Scrub')).isPresent()).toBe(true);
+        expect(element(by.cssContainingText('button', 'Cluster-wide configuration')).isPresent()).toBe(true);
+      });
+      it('should check the number of tabs when selecting an osd is correct', () => {
+        osds.navigateTo();
+        $$('.datatable-body-cell-label')
+          .first()
+          .click(); // clicks first osd
+        expect(osds.getTabsCount()).toEqual(7); // includes tabs at the top of the page
+      });
+      it('should show the correct text for the tab labels', () => {
+        expect(osds.getTabText(2)).toEqual('Attributes (OSD map)');
+        expect(osds.getTabText(3)).toEqual('Metadata');
+        expect(osds.getTabText(4)).toEqual('Performance counter');
+        expect(osds.getTabText(5)).toEqual('Histogram');
+        expect(osds.getTabText(6)).toEqual('Performance Details');
+      });
   });
 });
--- src/pybind/mgr/dashboard/frontend/e2e/cluster/osds.po.ts
+++ src/pybind/mgr/dashboard/frontend/e2e/cluster/osds.po.ts
@@ -1,5 +1,10 @@
+import { $, $$, browser, by, element } from 'protractor';
 import { PageHelper } from '../page-helper.po';
 
 export class OSDsPageHelper extends PageHelper {
   pages = { index: '/#/osd' };
+
+  getTableRows() {
+    return element.all(by.css('datatable-row-wrapper'));
+  }
 }
